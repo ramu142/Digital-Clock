7829dedf1a3db0f0571432affc72253138566fa6
ramu142 <57120478+ramu142@users.noreply.github.com>
2021-06-07 06:36:53 +0530

    Add files via upload

---
+++ CLOCK.py
@@ -0,0 +1,19 @@
+from time import strftime
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+root.title('Clock')
+
+
+def time():
+    string = strftime("%H:%M:%S %p")
+    lable.config(text=string)
+    lable.after(1000, time)
+
+
+lable = Label(root, font=("BroadcastTitling.ttf", 80), background="black", foreground="cyan")
+lable.pack(anchor="center")
+time()
+
+root.mainloop()
